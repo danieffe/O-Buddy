19d7125b8c67068eedc6931fd0212f7e25e8742a
Simone Di Blasi <diblasisimone925@gmail.com>
2025-05-28 09:35:03 +0200

    ..

---
+++ O-Buddy/Views/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  O-Buddy
+//
+//  Created by Simone Di Blasi on 27/05/25.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
